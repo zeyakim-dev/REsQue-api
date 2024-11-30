1fe956483f855b8810cf8000245815a6a9248802
zeyakim-dev <chlwltjrdev@gmail.com>
2024-11-30 15:58:28 +0900

    🎨__init__.py 파일 일괄 생성

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return {'message': 'Hello, DDD!'}
 
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    return {"message": "Hello, DDD!"}
+
+
+if __name__ == "__main__":
     app.run(debug=True)
