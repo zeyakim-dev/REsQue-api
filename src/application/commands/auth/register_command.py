2696358ab3315276a20c5dc96e78fe679ad6e8ca
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-04 00:29:06 +0900

    ✨ RegisterCommand
    회원가입 커맨드 구현

--- src/application/commands/auth/register_command.py
+++ src/application/commands/auth/register_command.py
@@ -1,31 +1,54 @@
 from dataclasses import dataclass
-from typing import Protocol
+from typing import Optional
+from uuid import UUID
 
+from src.application.commands.command import Command
+from src.application.ports.repositories.user.user_repository import UserRepository
+from src.application.ports.uow import UnitOfWork
+from src.domain.user.user import User
 from src.infrastructure.security.password_hasher import PasswordHasher
 from src.infrastructure.uuid.uuid_generator import UUIDv7Generator
-from src.domain.user.user import User
-
-
-class UserRepository(Protocol):
-    def save(self, user: User) -> None:
-        pass
-
 
 @dataclass(frozen=True)
-class RegisterCommand:
+class RegisterCommand(Command[UUID]):
+    """새로운 사용자를 등록하는 커맨드입니다."""
+    
     username: str
     password: str
-
-    def execute(
-        self,
-        user_repository: UserRepository,
-        id_generator: UUIDv7Generator,
-        password_hasher: PasswordHasher,
-    ) -> None:
-        new_user = User.create(
-            user_info=self.__dict__,
-            password_hasher=password_hasher,
-            id_generator=id_generator,
-        )
-
-        user_repository.save(new_user)
+    _password_hasher: PasswordHasher
+    _id_generator: UUIDv7Generator
+    
+    def execute(self, uow: UnitOfWork) -> UUID:
+        """사용자 등록을 수행합니다.
+        
+        트랜잭션 범위 내에서 사용자 생성과 저장을 수행하며,
+        모든 데이터베이스 작업이 원자적으로 처리됩니다.
+        
+        Args:
+            uow: 트랜잭션 관리를 위한 UnitOfWork 인스턴스
+            
+        Returns:
+            UUID: 생성된 사용자의 ID
+            
+        Raises:
+            ValueError: 사용자명이 이미 존재하는 경우
+        """
+        with uow:
+            user_repository: UserRepository = uow.get_repository(UserRepository)
+            
+            # 사용자명 중복 검사
+            if user_repository.exists_by_username(self.username):
+                raise ValueError("이미 존재하는 사용자명입니다")
+            
+            # 새로운 사용자 생성
+            new_user = User.create(
+                user_info=self.__dict__,
+                password_hasher=self._password_hasher,
+                id_generator=self._id_generator
+            )
+            
+            # 저장
+            user_repository.add(new_user)
+            uow.commit()
+            
+            return new_user.id
