7c0d7b94f3d995eda36d1a0dc125074cc906ea01
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-04 00:04:21 +0900

    ✨ User Model

--- src/infrastructure/persistence/sqlalchemy/models/base.py
+++ src/infrastructure/persistence/sqlalchemy/models/base.py
@@ -1,4 +1,4 @@
 from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    ...
+
+class Base(DeclarativeBase): ...
--- src/infrastructure/persistence/sqlalchemy/models/users.py
+++ src/infrastructure/persistence/sqlalchemy/models/users.py
@@ -1,20 +1,18 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.dialects.postgresql import UUID
 from uuid import UUID as PyUUID
-from src.infrastructure.persistence.sqlalchemy.models.base import Base
+
 from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructure.persistence.sqlalchemy.models.base import Base
+
 
 class UserModel(Base):
-    __tablename__ = 'users'
-    
+    __tablename__ = "users"
+
     id: Mapped[PyUUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    username: Mapped[str] = mapped_column(
-        String(50), 
-        unique=True
-    )
-    hashed_password: Mapped[str] = mapped_column(
-        String(255)
-    )
-    
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+
     def __repr__(self) -> str:
         return f"User(id={self.id}, username={self.username})"
