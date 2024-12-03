86b2e38bd69687c83af371fd329a8954e6990d84
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-04 00:03:00 +0900

    ✨ UserRepository
    추상 유저 레포지토리 클래스

--- src/application/ports/repositories/user/user_repository.py
+++ src/application/ports/repositories/user/user_repository.py
@@ -4,14 +4,15 @@ from typing import Optional
 from src.application.ports.repositories.repository import Repository
 from src.domain.user.user import User
 
+
 class UserRepository(Repository[User]):
     """User 엔티티를 위한 Repository"""
-    
+
     @abstractmethod
     def find_by_username(self, username: str) -> Optional[User]:
         """사용자명으로 사용자 조회"""
         pass
-    
+
     @abstractmethod
     def exists_by_username(self, username: str) -> bool:
         """사용자명 존재 여부 확인"""
