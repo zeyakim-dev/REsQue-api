10fccb1fce9256ff7dbead7e10ea9c88cbed622c
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-06 12:02:47 +0900

    🎨 Bootstrap 코드에서 yaml로 설정 읽기

--- src/bootstrap/bootstrap.py
+++ src/bootstrap/bootstrap.py
@@ -1,34 +1,35 @@
-from typing import Dict
 from flask import Flask
-from dependency_injector.wiring import inject, Provide
 from src.infrastructure.container import Container
+from src.infrastructure.flask.extensions import FlaskContainer
 from src.interfaces.api import api
 
-
 class Bootstrap:
-    """DDD 애플리케이션의 부트스트랩을 담당하는 클래스입니다."""
+    """애플리케이션의 부트스트래핑을 담당하는 클래스입니다."""
 
-    def __init__(self, config: Dict):
+    def __init__(self, config_path: str):
         self.container = Container()
-        self.container.config.from_dict(config)
+        self.container.config.from_yaml(str(config_path))
+
+        # 의존성 주입 설정
         self.container.wire(packages=[
             "src.interfaces",
             "src.application",
             "src.infrastructure"
         ])
-
+        
+        # Flask 확장 초기화
+        self.flask_container = FlaskContainer()
+        
     def create_app(self) -> Flask:
-        """Flask 애플리케이션을 생성하고 초기화합니다."""
         app = Flask(__name__)
         
-        # 컨테이너를 Flask 앱에 연결
-        app.container = self.container
+        # 컨테이너 확장 초기화
+        self.flask_container.init_app(app, self.container)
         
         # 블루프린트 등록
         self._register_blueprints(app)
         
         return app
-
+        
     def _register_blueprints(self, app: Flask):
-        """블루프린트를 등록합니다."""
         app.register_blueprint(api)
