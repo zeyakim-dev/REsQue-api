e40767236895ca0d1e9466a046c516375bad808b
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-07 11:48:50 +0900

    ✨ 인프라에 구애받지 않는 DatabaseFactory

--- src/infrastructure/persistence/database_factory.py
+++ src/infrastructure/persistence/database_factory.py
@@ -1,23 +1,19 @@
-from typing import Dict
+from abc import ABC, abstractmethod
+from typing import Dict, Type
 
-from src.infrastructure.persistence.sqlalchemy.config import create_sqlite_engine
-
-class DatabaseFactory:
-    engine_factory = {
-        'sqlite': create_sqlite_engine
-    }
+class DatabaseFactory(ABC):
+    def __init__(self, configuration: Dict):
+        self.config = configuration
     
-    @classmethod
-    def create_engine(cls, configuration: Dict):
-        db_section = configuration['persistence']
-        db_type = db_section['type']
-        db_config = db_section['config']
+    @abstractmethod
+    def create_engine(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def create_session_factory(self, db_engine):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def create_uow(self, session_factory):
+        raise NotImplementedError
 
-        creator = cls.engine_factory.get(db_type)
-        if not creator:
-            raise ValueError(f"Unsupported database type: {db_type}")
-
-        return creator(db_config)
-
-class SessionFactoryFactory:
-    ...
