3370728d22adbd1f6f73f84b6f9a8e0efd72b22c
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-07 11:49:17 +0900

    ✨ SQLAlchemyDatabaseFactory

--- src/infrastructure/persistence/sqlalchemy/config.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from sqlalchemy import create_engine
-
-def create_sqlite_engine(config: Dict):
-    return create_engine(
-        url=config['url'],
-        connect_args=config['connect_args']
-    )
