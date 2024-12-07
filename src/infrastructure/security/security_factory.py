b81e1f691c5c3abbbe2e648654f1abefa47d2acc
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-07 12:07:09 +0900

    ✨ SecurityFactory

--- src/infrastructure/security/security_factory.py
+++ src/infrastructure/security/security_factory.py
@@ -11,9 +11,8 @@ class SecurityFactory:
 
     @classmethod
     def create_password_hashser(cls, configuration: Dict) -> PasswordHasher:
-        password_hasher_section = configuration['security']['password_hasher']
-        password_hasher_type = password_hasher_section['type']
-        password_hasher_config = password_hasher_section['config']
+        password_hasher_type = configuration['type']
+        password_hasher_config = configuration['config']
 
         creator = cls.password_hasher_factory.get(password_hasher_type)
         if not creator:
@@ -27,9 +26,8 @@ class SecurityFactory:
     
     @classmethod
     def create_jwt_token_generator(cls, configuration: Dict) -> JWTTokenGenerator:
-        jwt_token_generator_section = configuration['security']['jwt_generator']
-        jwt_token_generator_type = jwt_token_generator_section['type']
-        jwt_token_generator_config = jwt_token_generator_section['config']
+        jwt_token_generator_type = configuration['type']
+        jwt_token_generator_config = configuration['config']
 
         creator = cls.jwt_token_generator_factory.get(jwt_token_generator_type)
         if not creator:
