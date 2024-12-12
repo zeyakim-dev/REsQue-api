f98e653928ce925201aecdc8c95ea523227ea42c
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-12 11:23:36 +0900

    🎨 위치 변경

--- src/domain/shared/aggregate_root.py
+++
@@ -1,5 +0,0 @@
-from src.domain.shared.entity import Entity
-
-
-class AggregateRoot(Entity):
-    pass
--- src/domain/shared/entity.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from uuid import UUID
-
-
-@dataclass(frozen=True)
-class Entity:
-    id: UUID
