189930813d33e13cd0c565aeb878413f60c8a269
zeyakim-dev <chlwltjrdev@gmail.com>
2024-12-16 11:16:58 +0900

    🎨 Entity를 Protocol -> ABC로 변경

--- src/domain/shared/base/entity.py
+++ src/domain/shared/base/entity.py
@@ -1,8 +1,9 @@
+from abc import ABC
 from dataclasses import dataclass
 from typing import Protocol
 from uuid import UUID
 
 
 @dataclass(frozen=True)
-class Entity(Protocol):
+class Entity(ABC):
     id: UUID
