d4c8bd24a6512862ed0808c2832759ae5f1533ed
zeyakim-dev <chlwltjrdev@gmail.com>
2025-02-01 10:49:48 +0900

    📝 Set up Sphinx documentation structure and configuration

---
+++ docs/sphinx/source/conf.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'REsQue-api'
+copyright = '2025, zeyakim-dev'
+author = 'zeyakim-dev'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
---
+++ docs/sphinx/source/design/index.rst
@@ -0,0 +1,14 @@
+.. _design-docs:
+
+시스템 설계 문서
+==================
+
+.. contents::
+   :depth: 2
+   :local:
+   :backlinks: top
+
+개요
+----
+본 문서는 REsQue-api 시스템의 설계 결정 과정과 아키텍처 진화 기록을 다룹니다.
+
