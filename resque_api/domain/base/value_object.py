3297a9f126498296e5df036737d08d01b538f104
zeyakim-dev <chlwltjrdev@gmail.com>
2025-02-22 19:21:10 +0900

    ♻️ Refactor: BaseVOCollection을 VOList에서 분리
    - 🔧 BaseVOCollection 추상 클래스를 VOList에서 분리하여 독립적인 클래스 구조로 개선
    - 📦 VOList는 BaseVOCollection을 상속받아 아이템 추가/제거 기능을 구현
    - 🔄 VOList에서 BaseVOCollection의 공통 기능을 상속받도록 변경
    - 🚀 향후 다른 컬렉션 클래스들이 BaseVOCollection을 상속하여 확장 가능하도록 설계

--- resque_api/domain/base/value_object.py
+++ resque_api/domain/base/value_object.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from dataclasses import dataclass, field
 from typing import Generic, Iterator, Self, TypeVar
 
@@ -17,23 +18,39 @@ class ValueObject(Generic[T]):
         return self.value == other.value
 
 
+class BaseVOCollection(Collection[T], Generic[T]):
+    """Value Object 컬렉션을 위한 기본 추상 클래스"""
+
+    values: Collection[T]
+
+    @abstractmethod
+    def add(self, item: T) -> Self:
+        """아이템을 컬렉션에 추가하고 새로운 컬렉션 반환"""
+        pass
+
+    @abstractmethod
+    def remove(self, item: T) -> Self:
+        """아이템을 컬렉션에서 제거하고 새로운 컬렉션 반환"""
+        pass
+
+    def __contains__(self, item: T) -> bool:
+        """해당 아이템이 컬렉션에 포함되어 있는지 확인"""
+        return item in self.values
+
+    def __len__(self) -> int:
+        """컬렉션의 길이 반환"""
+        return len(self.values)
+
+    def __iter__(self) -> Iterator[T]:
+        """컬렉션 순회"""
+        return iter(self.values)
+
+
 @dataclass(frozen=True)
 class VOList(Collection[T], Generic[T]):
     """VO의 리스트를 관리하는 불변 컬렉션"""
 
     values: tuple[T, ...] = field(default_factory=tuple)
-
-    def __iter__(self) -> Iterator[T]:
-        """리스트 순회 지원"""
-        return iter(self.values)
-
-    def __contains__(self, item: T) -> bool:
-        """해당 아이템이 리스트에 존재하는지 확인"""
-        return item in self.values
-
-    def __len__(self) -> int:
-        """리스트 길이 반환"""
-        return len(self.values)
 
     def add(self, item: T) -> Self:
         """새로운 아이템을 추가한 새로운 VOList 반환 (불변 유지)"""
