9f18fed724f42025fe6d75d4847cf924b4a6363f
zeyakim-dev <chlwltjrdev@gmail.com>
2025-02-24 20:26:56 +0900

    🎨  feat: publish 함수 개선
    - 메시지 타입에 대해 등록된 핸들러가 없을 경우 예외 메시지 명확화
    - UOW로부터 pop_events()로 가져온 이벤트들을 event_queue에 확장하여 추가하는 로직 개선
    - 코드 가독성 및 에러 처리 강화

--- resque_api/application/message/bus/message_bus.py
+++ resque_api/application/message/bus/message_bus.py
@@ -3,12 +3,14 @@ from typing import Type
 from resque_api.application.message.command.base.command_handler import CommandHandler
 from resque_api.application.message.common.message import Message
 from resque_api.application.message.event.base.event_handler import EventHandler
+from resque_api.application.ports.uow import UnitOfWork
 
 
 class MessageBus:
-    def __init__(self):
+    def __init__(self, uow: UnitOfWork):
         self.handlers: dict[Type[Message], CommandHandler | EventHandler] = dict()
         self.event_queue = []
+        self.uow = uow
 
     def subscribe(self, handler: CommandHandler | EventHandler, message_type: Type[Message]):
         self.handlers[message_type] = handler
@@ -16,6 +18,12 @@ class MessageBus:
     def publish(self, message: Message):
         handler = self.handlers.get(type(message))
         
-        if not handler:
-            raise Exception
-        return handler.handle(message)
+        if handler is None:
+            raise Exception(f"등록된 핸들러가 없습니다. 메시지 타입: {type(message).__name__}")
+        
+        result = handler.handle(message, self.uow)
+        events = self.uow.pop_events()
+
+        if events:
+            self.event_queue.extend(events)
+        return result
