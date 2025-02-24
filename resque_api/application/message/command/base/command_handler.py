c0984d19c2fdb47a230ad01b28cf662151e39c17
zeyakim-dev <chlwltjrdev@gmail.com>
2025-02-24 20:26:32 +0900

    🔧 feat: CommandHandler, EventHandler 변경
    - `handle` 메서드에 UOW를 인자로 추가하여 트랜잭션 관리를 도입

--- resque_api/application/message/command/base/command_handler.py
+++ resque_api/application/message/command/base/command_handler.py
@@ -1,9 +1,10 @@
 from typing import Any, Generic, Protocol, TypeVar
 
 from resque_api.application.message.command.base.command import Command
+from resque_api.application.ports.uow import UnitOfWork
 
 C = TypeVar('C', bound=Command)
 
 class CommandHandler(Protocol, Generic[C]):
-    def handle(self, command: C) -> Any:
+    def handle(self, command: C, uow: UnitOfWork) -> Any:
         ...
